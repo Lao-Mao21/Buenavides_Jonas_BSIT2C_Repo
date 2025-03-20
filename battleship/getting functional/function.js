0c79d5ad45042cd128219c5c63a7dc792fb4cc47
unknown <buenavidesjonas@gmail.com>
2025-03-20 15:28:56 +0800

    new updates function

---
+++ battleship/getting functional/function.js
@@ -0,0 +1,53 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        cosole.log(name + " says woof woof");
+    }    
+}
+
+bark("rover", 24);
+bark("dog ningen", 500);
+break("human dog", 250);
+
+/*
+var dogName = "rover";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    cosole.log(dogName + " says woof woof");
+}
+
+var dogName = "oblast";
+var dogWeight = 30;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    cosole.log(dogName + " says woof woof");
+}
+
+var dogName = "oplan";
+var dogWeight = 21;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    cosole.log(dogName + " says woof woof");
+}
+
+var dogName = "revere";
+var dogWeight = 27;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    cosole.log(dogName + " says woof woof");
+}
+
+var dogName = "Human";
+var dogWeight = 40;
+if (dogWeight > 20) {
+    console.log(dogName + " says salamalekum sala!");
+} else {
+    cosole.log(dogName + " says waai waaai waait");
+}*/
+    
