93478d92a1ffcb8d791045d74d509dd96d7ed720
Nas_0o0 <BuenavidesJonas@gmail.com>
2025-03-27 16:51:48 +0800

    New activity

---
+++ return/return.js
@@ -0,0 +1,7 @@
+function printName(firstName, lastName) {
+    return firstName + " " + lastName; 
+}
+var fullName = printName("Jonas", "Buenavides");
+console.log(printName("My Name is " + fullName));
+var result = "My Name is " + fullName;
+alert(result);
